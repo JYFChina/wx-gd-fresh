c517e4768b1ae5a199be65d2552b05f66e7a362b
JYF <643905028@qq.com>
2019-04-15 06:23:18 +0800

    主页轮廓优化
    js编写

--- pages/commodity/commodity.js
+++ pages/commodity/commodity.js
@@ -1,6 +1,6 @@
 Page({
   data: {
-    background: ['demo-text-1', 'demo-text-2', 'demo-text-3','demo-text-4'],
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3', 'demo-text-4'],
     indicatorDots: true,
     vertical: false,
     autoplay: false,
@@ -10,31 +10,55 @@ Page({
     previousMargin: 0,
     nextMargin: 0
   },
-  changeProperty: function (e) {
+  onChange(event) {
+    if (event.detail == 0) {
+      console.log("主页面")
+      wx.navigateTo({
+        url: './test',
+      })
+      
+    }
+    if (event.detail == 1) {
+      console.log("搜索")
+    }
+    if (event.detail == 2) {
+      console.log("购物车")
+    }
+    if (event.detail == 3) {
+      console.log("我的")
+    }
+    console.log(event.detail);
+  },
+  changeProperty: function(e) {
     var propertyName = e.currentTarget.dataset.propertyName
     var newData = {}
     newData[propertyName] = e.detail.value
     this.setData(newData)
   },
-  changeIndicatorDots: function (e) {
+  changeIndicatorDots: function(e) {
     this.setData({
       indicatorDots: !this.data.indicatorDots
     })
   },
-  changeAutoplay: function (e) {
+  changeAutoplay: function(e) {
     this.setData({
       autoplay: !this.data.autoplay
     })
   },
-  intervalChange: function (e) {
+  intervalChange: function(e) {
     this.setData({
       interval: e.detail.value
     })
   },
-  durationChange: function (e) {
+  durationChange: function(e) {
     this.setData({
       duration: e.detail.value
     })
+  },
+  go: function() {
+    wx.redirectTo({
+      url: '../goodsaction/goodsaction',
+    })
   }
-  
+
 })
