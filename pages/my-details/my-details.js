bf9471302a2d47bc0569e837a92035db5a979dfc
JYF <643905028@qq.com>
2019-04-15 19:45:30 +0800

    个人中心
    样式基本完成

--- pages/my-details/my-details.js
+++ pages/my-details/my-details.js
@@ -1,4 +1,6 @@
 // pages/my-details /my-details.js
+
+
 Page({
 
   /**
@@ -6,61 +8,123 @@ Page({
    */
   data: {
 
-  },
+    user: {
 
+    }
+  },
+  onSuccess(res) {
+    console.log(res.detail)
+  },
+  onFail(res) {
+    console.log(res)
+  },
+  go: function() {
+    console.log(this.user)
+
+  },
+  getPhoneNumber(e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.iv)
+    console.log(e.detail.encryptedData)
+  },
+  onGotUserInfo(e) {
+    console.log("E的值为:============" + e)
+    console.log(e);
+    this.setData({
+      "user.city": e.detail.userInfo.city,
+      "user.nickName": e.detail.userInfo.nickName,
+      "user.country": e.detail.userInfo.country,
+      "user.avatarUrl": e.detail.userInfo.avatarUrl,
+      "user.province": e.detail.userInfo.province
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    var that = this;
+
+    // wx.login({
+    //   success: res => {
+    //     // ------ 获取凭证 ------
+    //     var code = res.code;
+    //     console.log(code)
+    //     if (code) {
+    //       // console.log('获取用户登录凭证：' + code);
+    //       // ------ 发送凭证 ------
+    //       wx.request({
+    //        // url: 'https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code',
+    //         data: { code: code },
+    //         method: 'POST',
+    //         header: {
+    //           'content-type': 'application/json'
+    //         },
+    //         success: function (res) {
+    //           if (res.statusCode == 200) {
+    //             // console.log("获取到的openid为：" + res.data)
+    //             // that.globalData.openid = res.data
+    //             wx.setStorageSync('openid', res.data)
+    //           } else {
+    //             console.log(res.errMsg)
+    //           }
+    //         },
+    //       })
+    //     } else {
+    //       console.log('获取用户登录失败：' + res.errMsg);
+    //     }
+    //   }
+    // })
+
+
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
