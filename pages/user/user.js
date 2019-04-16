6961fef8fa394ee0765a043255c45416a255fca4
JYF <643905028@qq.com>
2019-04-16 17:32:28 +0800

    微信登陆未完成
    撒啊发给你

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,59 +1,84 @@
 // pages/user/user.js
-var app = getApp()
-Page( {
-  data: {
-    userInfo: {},
-    orderInfo:{},
-    projectSource: 'https://github.com/liuxuanqiang/wechat-weapp-mall',
-    userListInfo: [ {
-        icon: '../../images/iconfont-dingdan.png',
-        text: '我的订单',
-        isunread: true,
-        unreadNum: 2
-      }, {
-        icon: '../../images/iconfont-card.png',
-        text: '我的代金券',
-        isunread: false,
-        unreadNum: 2
-      }, {
-        icon: '../../images/iconfont-icontuan.png',
-        text: '我的拼团',
-        isunread: true,
-        unreadNum: 1
-      }, {
-        icon: '../../images/iconfont-shouhuodizhi.png',
-        text: '收货地址管理'
-      }, {
-        icon: '../../images/iconfont-kefu.png',
-        text: '联系客服'
-      }, {
-        icon: '../../images/iconfont-help.png',
-        text: '常见问题'
-      }],
-       loadingText: '加载中...',
-       loadingHidden: false,
-  },
-  onLoad: function () {
-      var that = this
-      //调用应用实例的方法获取全局数据
-    
 
-      this.loadOrderStatus();
+var app = getApp()
+Page({
+  data: {
+
+    userInfo: {},
+    orderInfo: {},
+    projectSource: '',
+    userListInfo: [{
+      icon: '../../images/iconfont-dingdan.png',
+      text: '我的订单',
+      isunread: true,
+      unreadNum: 2
+    }, {
+      icon: '../../images/iconfont-card.png',
+      text: '我的代金券',
+      isunread: false,
+      unreadNum: 2
+    }, {
+      icon: '../../images/iconfont-icontuan.png',
+      text: '我的拼团',
+      isunread: true,
+      unreadNum: 1
+    }, {
+      icon: '../../images/iconfont-shouhuodizhi.png',
+      text: '收货地址管理'
+    }, {
+      icon: '../../images/iconfont-kefu.png',
+      text: '联系客服'
+    }, {
+      icon: '../../images/iconfont-help.png',
+      text: '常见问题'
+    }],
+    loadingText: '加载中...',
+    loadingHidden: false,
   },
-  onShow:function(){
+  onLoad: function(options) {
+
+
+
+  },
+  onGotUserInfo: function(e) {
+
+    console.log(e.detail.userInfo)
+    wx.login({
+      success: function(res) {
+        //后去登陆后的临时凭证
+        var code = res.code;
+        console.log(res)
+        wx.request({
+          url: 'http://192.168.43.5/wxlogin.do?code='+code,
+          method: "post",         
+          success: function(result) {
+            console.log(result);
+          }
+
+        })
+      }
+    })
+    this.setData({
+      "userInfo": e.detail.userInfo
+    })
+  },
+  bindGetUserInfo(e) {
+    console.log(e.detail.userInfo)
+  },
+  onShow: function() {
     this.loadOrderStatus();
   },
-  loadOrderStatus:function(){
-   
+  loadOrderStatus: function() {
+
   },
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
     return {
       title: '宠物美容学校',
       path: '/pages/index/index',
-      success: function (res) {
+      success: function(res) {
         // 分享成功
       },
-      fail: function (res) {
+      fail: function(res) {
         // 分享失败
       }
     }
